b1fbcac2ea098e64585d5f577a5728dcbaf6507b
YuXing <a0975708860@gmail.com>
2023-05-16 22:36:12 +0800

    chore: initialize users' data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.insert_all([
+    { name: 'YuXing' },
+    { name: 'Star'  },
+    { name: 'Simon' }
+  ])
+
+pp 'Finish'
