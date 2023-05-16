fb550f0d83a1c72b81fdfd6dfe894166e11ec03a
YuXing <a0975708860@gmail.com>
2023-05-16 17:45:38 +0800

    chore: add annotate gem and update users' information

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string(40)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
