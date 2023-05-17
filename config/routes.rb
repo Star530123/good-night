138e856deb35d57b83c31201a7763e571e179c0c
YuXing <a0975708860@gmail.com>
2023-05-18 00:20:37 +0800

    refactor: collect routes by scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  post 'sleep/clock_in', to: 'sleep#clock_in'
-  post 'sleep/clock_out', to: 'sleep#clock_out'
-  get 'sleep/clocked_in_times', to: 'sleep#clocked_in_times'
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
-  post 'users/follow', to: 'users#follow'
-  post 'users/unfollow', to: 'users#unfollow'
-  get 'sleep/following_user_records', to: 'sleep#following_user_records'
+  scope 'users' do
+    get '', to: 'users#index'
+    post '', to: 'users#create'
+    post 'follow', to: 'users#follow'
+    post 'unfollow', to: 'users#unfollow'
+  end
+
+  scope 'sleep' do
+    post 'clock_in', to: 'sleep#clock_in'
+    post 'clock_out', to: 'sleep#clock_out'
+    get 'clocked_in_times', to: 'sleep#clocked_in_times'
+    get 'following_user_records', to: 'sleep#following_user_records'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
