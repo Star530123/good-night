3bd7abb59599b53a53111b8bc1fc2b7d505dd2f6
YuXing <a0975708860@gmail.com>
2023-05-20 14:29:01 +0800

    refactor: remove attr_reader in clock_in_service

--- app/services/sleep/clock_in_service.rb
+++ app/services/sleep/clock_in_service.rb
@@ -1,15 +1,14 @@
 class Sleep::ClockInService < BaseService
-  attr_reader :user
-  
+
   def initialize(user:)
     super()
     @user = user
   end
 
   def execute
-    sleep_record = user.sleep_records.last
+    sleep_record = @user.sleep_records.last
     raise "You haven't clocked out!" if sleep_record && sleep_record.clock_out.nil?
 
-    SleepRecord.create(user: user, clock_in: Time.now)
+    SleepRecord.create(user: @user, clock_in: Time.now)
   end
 end
