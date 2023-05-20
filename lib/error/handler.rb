7115532247f234cb16f45d42e3982f958c9546aa
YuXing <a0975708860@gmail.com>
2023-05-20 08:36:17 +0800

    feat: add base service

--- lib/error/handler.rb
+++ lib/error/handler.rb
@@ -2,18 +2,27 @@ module Error
   module Handler
     def self.included(clazz)
       clazz.class_eval do
+        rescue_from NotImplementedError, with: :non_implement_error
         rescue_from StandardError, with: :standard_error
       end
     end
 
     private
 
+    def non_implement_error(e)
+      render_error_message(error: :non_implement_error, status: 500, message: e.to_s)
+    end
+
     def standard_error(e)
+      render_error_message(error: :standard_error, status: 500, message: e.to_s)
+    end
+
+    def render_error_message(error:, status:, message:)
       render json: {
-        error: :standard_error,
-        status: 500,
-        message: e.to_s
-      }, status: 500
+        error: error,
+        status: status,
+        message: message
+      }, status: status
     end
   end
 end
